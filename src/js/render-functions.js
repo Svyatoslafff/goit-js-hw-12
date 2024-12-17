cc1cf547a88d030141bb503431a2cd940982fbc5
Svyatoslafff <svjatoslavkom12@gmail.com>
2024-12-17 18:42:19 +0200

    update render-functions.js

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -9,27 +9,33 @@ let gallery = new SimpleLightbox('.gallery a');
 
 export default function renderImages(imagesArray) {
     const imagesCode = imagesArray
-        .map(image => {
+        .map(({largeImageURL, webformatURL, tags, likes, views, comments, downloads}) => {
         return `<li class="image-container">
-                    <a class="image-link" href="${image.largeImageURL}">
-                        <img class="image" width="360" height="240" src="${image.webformatURL}" alt="${image.tags}"/>
+                    <a class="image-link" href="${largeImageURL}">
+                        <img
+                            class="image"
+                            width="360"
+                            height="240"
+                            src="${webformatURL}" 
+                            alt="${tags}"
+                        />
                     </a>
                     <ul class="image-description">
                         <li>
                             <h3>Likes</h3>
-                            <p>${image.likes}</p>
+                            <p>${likes}</p>
                         </li>
                         <li>
                             <h3>Views</h3>
-                            <p>${image.views}</p>
+                            <p>${views}</p>
                         </li>
                         <li>
                             <h3>Comments</h3>
-                            <p>${image.comments}</p>
+                            <p>${comments}</p>
                         </li>
                         <li>
                             <h3>Downloads</h3>
-                            <p>${image.downloads}</p>
+                            <p>${downloads}</p>
                         </li>
                     </ul>
                 </li>` 
